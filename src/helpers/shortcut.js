54760b74494fce29a04607f363f5816bba31be5a
pftom <1043269994@qq.com>
2020-01-21 20:48:53 +0800

    实现了触发器触发渲染内联样式

--- src/helpers/shortcut.js
+++ src/helpers/shortcut.js
@@ -1,6 +1,11 @@
 import { Range, Editor, Transforms, Point } from "slate";
 
-const SHORTCUTS = {
+import { toggleCodeMark } from "./code";
+import { toggleBoldMark } from "./bold";
+import { toggleItalicMark } from "./italic";
+import { toggleStrikethroughMark } from "./strikethrough";
+
+const UNARY_SHORTCUTS = {
   "*": "list-item",
   "-": "list-item",
   "+": "list-item",
@@ -13,6 +18,20 @@ const SHORTCUTS = {
   "######": "heading-six"
 };
 
+const BINARY_SHORTCUTS = [
+  "code",
+  "bold",
+  "italic",
+  "strikethrough"
+]
+
+const BINARY_SHORTCUTS_REGEX = [
+  "`([^`]+)`",
+  "\\*\\*([^\\*]+)\\*\\*",
+  "\\*([^\\*]+)\\*",
+  "~~([^~]+)~~"
+];
+
 export const withShortcuts = editor => {
   const { deleteBackward, insertText } = editor;
 
@@ -24,11 +43,20 @@ export const withShortcuts = editor => {
       const match = Editor.above(editor, {
         match: n => Editor.isBlock(editor, n)
       });
+
+      console.log("editor", editor);
+      console.log("match", match);
+
       const path = match ? match[1] : [];
       const start = Editor.start(editor, path);
       const range = { anchor, focus: start };
       const beforeText = Editor.string(editor, range);
-      const type = SHORTCUTS[beforeText];
+
+      console.log("start", start);
+      console.log("anchor", anchor);
+      console.log("beforeText", beforeText);
+
+      const type = UNARY_SHORTCUTS[beforeText];
 
       if (type) {
         Transforms.select(editor, range);
@@ -48,8 +76,105 @@ export const withShortcuts = editor => {
 
         return;
       }
-    }
 
+      let matchArr;
+      let regex;
+      let format;
+
+      BINARY_SHORTCUTS_REGEX.map((regexStr, index) => {
+        if (format) {
+          return;
+        }
+
+        regex = new RegExp(regexStr, "g");
+        
+        if (regex.test(beforeText)) {
+          format = BINARY_SHORTCUTS[index]
+        }
+      });
+
+      if (format) {
+        matchArr = beforeText.match(regex);
+      }
+
+      if (matchArr) {
+        const targetTextWithMdTag = matchArr[matchArr.length - 1];
+
+        // 删除逻辑
+        const deleteRangeStartOffset =
+          beforeText.length - targetTextWithMdTag.length;
+        const deleteRangeEndOffset = beforeText.length;
+
+        let deleteRangeStart = Editor.start(editor, path);
+        deleteRangeStart = {
+          ...deleteRangeStart,
+          offset: deleteRangeStartOffset
+        };
+
+        let deleteRangeEnd = Editor.start(editor, path);
+        deleteRangeEnd = { ...deleteRangeEnd, offset: deleteRangeEndOffset };
+
+        const deleteRange = { anchor: deleteRangeStart, focus: deleteRangeEnd };
+        Transforms.select(editor, deleteRange);
+        Transforms.delete(editor);
+
+        // 插入新的内容
+        const targetTextArr = regex.exec(targetTextWithMdTag);
+        const targetInsertText = targetTextArr[1];
+        insertText(targetInsertText);
+
+        // 开始对新内容进行标注
+        const needMarkRangeStartOffset = deleteRangeStartOffset;
+        const needMarkRangeEndOffset =
+          needMarkRangeStartOffset + targetInsertText.length;
+        let needMarkRangeStart = Editor.start(editor, path);
+        needMarkRangeStart = {
+          ...needMarkRangeStart,
+          offset: needMarkRangeStartOffset
+        };
+
+        let needMarkRangeEnd = Editor.start(editor, path);
+        needMarkRangeEnd = {
+          ...needMarkRangeEnd,
+          offset: needMarkRangeEndOffset
+        };
+
+        const needMarkRange = {
+          anchor: needMarkRangeStart,
+          focus: needMarkRangeEnd
+        };
+
+        Transforms.select(editor, needMarkRange);
+
+        switch (format) {
+          case "code": {
+            toggleCodeMark(editor);
+            break;
+          }
+
+          case "bold": {
+            toggleBoldMark(editor);
+            break;
+          }
+
+          case "italic": {
+            toggleItalicMark(editor);
+            break;
+          }
+
+          case "strikethrough": {
+            toggleStrikethroughMark(editor);
+            break;
+          }
+        }
+
+        Transforms.collapse(editor, {
+          edge: "focus"
+        });
+
+        return;
+      }
+    }
     insertText(text);
   };
 
