7cf3e00ef2ed73f83b1b773f5093b97078827dc7
pftom <1043269994@qq.com>
2020-01-24 16:55:06 +0800

    添加多语言支持

--- src/elements/codeBlock.js
+++ src/elements/codeBlock.js
@@ -2,6 +2,72 @@ import React, { useState, useEffect, useCallback } from "react";
 import { useSlate } from "slate-react";
 import { Transforms, Editor, Element } from "slate";
 
+const languages = [
+  "Plain Text",
+  "Bash",
+  "Basic",
+  "C",
+  "C++",
+  "C#",
+  "CSS",
+  "Dart",
+  "Diff",
+  "Dockerfile",
+  "Erlang",
+  "Git",
+  "Go",
+  "GraphQL",
+  "Groovy",
+  "HTML",
+  "HTTP",
+  "Java",
+  "JavaScript",
+  "JSON",
+  "JSX",
+  "KaTeX",
+  "Kotlin",
+  "Less",
+  "Makefile",
+  "Markdown",
+  "MATLAB",
+  "Nginx",
+  "Objective-C",
+  "Pascal",
+  "Perl",
+  "PHP",
+  "PowerShell",
+  "Protobuf",
+  "Python",
+  "R",
+  "Ruby",
+  "Rust",
+  "Scala",
+  "Shell",
+  "SQL",
+  "PL/SQL",
+  "Swift",
+  "TypeScript",
+  "VB.net",
+  "Velocity",
+  "XML",
+  "YAML",
+  "sTeX",
+  "LaTeX",
+  "SystemVerilog",
+  "Tcl",
+  "Verilog",
+  "Vue",
+  "Lua"
+];
+
+const mapOptionToValue = optionLang => {
+  let optionValue = optionLang
+    .toLocaleLowerCase()
+    .replace(/(\-)|(\/)|(\.)/g, "");
+
+  return optionValue;
+};
+
 const CodeBlockElement = props => {
   const { element } = props;
   const { lang: defaultLang = "Plain Text" } = element;
@@ -24,10 +90,9 @@ const CodeBlockElement = props => {
   return (
     <div {...props.attributes}>
       <select contentEditable={false} value={lang} onChange={handleChange}>
-        <option value="text">Plain Text</option>
-        <option value="markup">HTML</option>
-        <option value="css">CSS</option>
-        <option value="javascript">JavaScript</option>
+        {languages.map(lang => (
+          <option value={mapOptionToValue(lang)}>{lang}</option>
+        ))}
       </select>
       <pre>
         <code lang={`language-${lang}`}>{props.children}</code>
