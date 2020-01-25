67caa2a720bb6f9b5123868816dacc9b46b006e2
mRcfps <1402491442@qq.com>
2020-01-25 22:21:13 +0800

    重构 Prism 语法高亮代码

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import { Slate, Editable, withReact } from "slate-react";
 import isHotkey from "is-hotkey";
 import { withHistory } from "slate-history";
 import { css } from "emotion";
-import Prism from "prismjs";
 
 import CustomEditor, { customPlugins } from "./helpers";
 import Element, { isBlockActive, toggleBlock } from "./blocks";
 import Leaf, { toggleMark } from "./marks";
+import highlight from "./utils/highlight";
 import {
   BOLD,
   ITALIC,
@@ -81,111 +81,8 @@ const App = () => {
 
   const renderElement = useCallback(Element, []);
   const renderLeaf = useCallback(Leaf, []);
-
   const decorate = useCallback(
-    ([node, path]) => {
-      const decorations = [];
-
-      if (!isBlockActive(editor, CODE_BLOCK)) {
-        return decorations;
-      }
-
-      const grammarName = node.lang;
-      const grammar = Prism.languages[grammarName];
-
-      if (!grammar) {
-        return [];
-      }
-
-      const texts = node.children.map(item => {
-        if (item.children) {
-          return item.children[0].text;
-        }
-
-        return item.text;
-      });
-      const blockText = texts.join("\n");
-      const tokens = Prism.tokenize(blockText, grammar);
-
-      let textStart = 0;
-      let textEnd = 0;
-
-      texts.forEach(text => {
-        textEnd = textStart + text.length;
-
-        let offset = 0;
-
-        function processToken(token, accu) {
-          accu = accu || "";
-
-          if (typeof token === "string") {
-            if (accu) {
-              const decoration = createDecoration({
-                text,
-                textStart,
-                textEnd,
-                start: offset,
-                end: offset + token.length,
-                className: `prism-token token ${accu}`
-              });
-
-              if (decoration) {
-                decorations.push(decoration);
-              }
-            }
-
-            offset += token.length;
-          } else {
-            accu = `${accu} ${token.type} ${token.alias || ""}`;
-
-            if (typeof token.content === "string") {
-              const decoration = createDecoration({
-                text,
-                textStart,
-                textEnd,
-                start: offset,
-                end: offset + token.content.length,
-                className: `prism-token token ${accu}`
-              });
-
-              if (decoration) {
-                decorations.push(decoration);
-              }
-
-              offset += token.content.length;
-            } else {
-              for (let i = 0; i < token.content.length; i += 1) {
-                processToken(token.content[i], accu);
-              }
-            }
-          }
-        }
-
-        tokens.forEach(processToken);
-        textStart = textEnd + 1;
-      });
-
-      function createDecoration({ text, textStart, textEnd, start, end, className }) {
-        if (start >= textEnd || end <= textStart) {
-          return null;
-        }
-
-        start = Math.max(start, textStart);
-        end = Math.min(end, textEnd);
-
-        start -= textStart;
-        end -= textStart;
-
-        return {
-          anchor: { path, offset: start },
-          focus: { path, offset: end },
-          className,
-          prismToken: true
-        };
-      }
-
-      return decorations;
-    },
+    args => (isBlockActive(editor, CODE_BLOCK) ? highlight(args) : []),
     [editor]
   );
 
