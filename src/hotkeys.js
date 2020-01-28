aa067a29180a4c9a588445132cd15b1a6c71ba4b
pftom <1043269994@qq.com>
2020-01-28 17:49:20 +0800

    拆分出增加/减少元素层级的函数

--- src/hotkeys.js
+++ src/hotkeys.js
@@ -4,6 +4,7 @@ import isHotkey from "is-hotkey";
 import { getBeforeText, getChildrenText } from "./utils";
 import { toggleMark } from "./marks";
 import { toggleBlock, isBlockActive } from "./blocks";
+import { decreaseItemDepth, increaseItemDepth } from "./plugins/list";
 import {
   BOLD,
   ITALIC,
@@ -81,42 +82,7 @@ function handleTabKey(editor, event) {
   ) {
     event.preventDefault();
 
-    const type = isBlockActive(editor, BULLETED_LIST) ? BULLETED_LIST : NUMBERED_LIST;
-
-    const [node, _] = Editor.above(editor, {
-      match: n => n.type === type
-    });
-    const { level = 0 } = node;
-
-    // 如果此时 ul 中 li 大于 1 个，那么分出此时的 li，将其变成  ul > li
-    if (node && node.children.length > 1) {
-      Transforms.liftNodes(editor, {
-        match: n => n.type === LIST_ITEM
-      });
-
-      Transforms.wrapNodes(
-        editor,
-        {
-          type,
-          level: Math.min(level + 1, 8)
-        },
-        {
-          macth: n => n.type === LIST_ITEM
-        }
-      );
-    } else {
-      const type = isBlockActive(editor, BULLETED_LIST) ? BULLETED_LIST : NUMBERED_LIST;
-
-      Transforms.setNodes(
-        editor,
-        {
-          level: Math.min(level + 1, 8)
-        },
-        {
-          match: n => n.type === type
-        }
-      );
-    }
+    increaseItemDepth(editor);
   } else if (
     beforeText.length &&
     (isBlockActive(editor, BULLETED_LIST) || isBlockActive(editor, NUMBERED_LIST))
@@ -135,55 +101,7 @@ function handleTabKey(editor, event) {
 function handleShiftTabKey(editor, event) {
   event.preventDefault();
   if (isBlockActive(editor, BULLETED_LIST) || isBlockActive(editor, NUMBERED_LIST)) {
-    const type = isBlockActive(editor, BULLETED_LIST) ? BULLETED_LIST : NUMBERED_LIST;
-    const [node, _] = Editor.above(editor, {
-      match: n => n.type === type
-    });
-    const { level = 0 } = node;
-
-    // 如果此 list-item 所属列表中元素个数大于1，
-    // 就要考虑将此元素单独提取出来，成为一个列表
-    if (node && node.children.length > 1) {
-      Transforms.liftNodes(editor, {
-        match: n => n.type === LIST_ITEM
-      });
-
-      Transforms.wrapNodes(
-        editor,
-        {
-          type,
-          level: Math.max(level - 1, 0)
-        },
-        {
-          macth: n => n.type === LIST_ITEM
-        }
-      );
-    } else {
-      // 设置层级
-      Transforms.setNodes(
-        editor,
-        {
-          level: Math.max(level - 1, 0)
-        },
-        {
-          match: n => n.type === type
-        }
-      );
-
-      // 判断如果此时回退层级和之前的同类型列表的 level 一致，就合并进此同类型列表
-      const [node, _] = Editor.previous(editor, {
-        match: n => n.type === type
-      });
-
-      if (node) {
-        const { level: previousLevel = 0 } = node;
-        if (previousLevel === Math.max(level - 1, 0)) {
-          Transforms.mergeNodes(editor, {
-            match: n => n.type === type
-          });
-        }
-      }
-    }
+    decreaseItemDepth(editor);
   }
 }
 
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,5 +2,6 @@ import { withLinks } from "./link";
 import { withImages } from "./image";
 import withHr from "./hr";
 import withShortcuts from "./shortcuts";
+import { withList } from "./list";
 
-export default [withLinks, withImages, withHr, withShortcuts];
+export default [withLinks, withImages, withHr, withShortcuts, withList];
