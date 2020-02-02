d89c7ec2b0b2095be9529520d9c56d23567827d8
mRcfps <1402491442@qq.com>
2020-02-02 15:56:41 +0800

    粘贴纯文本到代码块中时，按换行符进行拆分

--- src/plugins/codeBlock.js
+++ src/plugins/codeBlock.js
@@ -125,15 +125,27 @@ export const withCodeBlock = editor => {
   editor.normalizeNode = entry => {
     const [node, path] = entry;
 
-    // 当前为根节点，无需处理
-    if (path.length === 0) {
-      return normalizeNode(entry);
-    }
-
     if (Element.isElement(node) && node.type === CODE_BLOCK) {
       for (const [child, childPath] of Node.children(editor, path)) {
         if (Element.isElement(child) && child.type !== CODE_LINE) {
           Transforms.setNodes(editor, { type: CODE_LINE }, { at: childPath });
+        }
+      }
+      return;
+    }
+
+    if (Element.isElement(node) && node.type === CODE_LINE) {
+      for (const [child, childPath] of Node.children(editor, path)) {
+        // 找到换行符，然后拆分 code-line节点
+        const newlineIndex = child.text.indexOf("\n");
+        if (newlineIndex >= 0) {
+          Transforms.splitNodes(editor, {
+            at: { path: childPath, offset: newlineIndex + 1 }
+          });
+          Transforms.select(editor, Editor.end(editor, childPath));
+          Editor.deleteBackward(editor);
+        } else {
+          Transforms.select(editor, Editor.end(editor, childPath));
         }
       }
       return;
