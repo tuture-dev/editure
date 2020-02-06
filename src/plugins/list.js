d74822615fc7e327c6b38a404709c8f09e975391
mRcfps <1402491442@qq.com>
2020-02-06 16:27:29 +0800

    Add "list-item should not have block children" constraint

--- src/plugins/list.js
+++ src/plugins/list.js
@@ -96,14 +96,31 @@ export const withList = editor => {
   editor.normalizeNode = entry => {
     const [node, path] = entry;
 
+    if (!Element.isElement(node)) {
+      return normalizeNode(entry);
+    }
+
+    if (node.type === BULLETED_LIST) {
+      for (const [child, childPath] of Node.children(editor, path)) {
+        const { level = 0, children } = child;
+        Transforms.setNodes(editor, { level, parent: node.type }, { at: childPath });
+
+        // List item should not have any block child.
+        if (children.length === 1 && Element.isElement(children[0])) {
+          Transforms.unwrapNodes(editor, { at: [...childPath, 0] });
+        }
+      }
+      return;
+    }
+
     // If the element is a numbered-list, ensure each item has correct number.
-    if (Element.isElement(node) && node.type === NUMBERED_LIST) {
+    if (node.type === NUMBERED_LIST) {
       const counterStack = [];
       let counter = 0;
       let lastLevel = 0;
 
       for (const [child, childPath] of Node.children(editor, path)) {
-        const { level = 0, number } = child;
+        const { level = 0, children } = child;
         if (level > lastLevel) {
           counterStack.push(counter);
           counter = 1;
@@ -116,13 +133,20 @@ export const withList = editor => {
           counter++;
         }
 
-        // Update item level and number if necessary.
-        if (number !== counter) {
-          Transforms.setNodes(editor, { level, number: counter }, { at: childPath });
-        }
+        Transforms.setNodes(
+          editor,
+          { level, parent: node.type, number: counter },
+          { at: childPath }
+        );
 
         lastLevel = level;
+
+        // List item should not have any block child.
+        if (children.length === 1 && Element.isElement(children[0])) {
+          Transforms.unwrapNodes(editor, { at: [...childPath, 0] });
+        }
       }
+      return;
     }
 
     // Fall back to the original `normalizeNode` to enforce other constraints.
