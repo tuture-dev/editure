c29a581523489142726929ca952023ab48eb2408
mRcfps <1402491442@qq.com>
2020-03-19 16:14:16 +0800

    Add more tests to cover list

--- packages/editure/src/plugins/__test__/list.test.ts
+++ packages/editure/src/plugins/__test__/list.test.ts
@@ -1,12 +1,11 @@
-import { Range, Editor } from 'tuture-slate';
+import { Range, Transforms, Editor } from 'tuture-slate';
 import * as F from 'editure-constants';
 
-import { EditorWithBlock } from '../base-block';
-import { withList } from '../list';
+import { withList, EditorWithList } from '../list';
 import { configureEditor, reset, inputText, deleteNTimes } from './utils';
 
 describe('withList', () => {
-  const editor = configureEditor({ blocks: [withList] }) as EditorWithBlock;
+  const editor = configureEditor({ blocks: [withList] }) as EditorWithList;
   reset(editor);
 
   afterEach(() => reset(editor));
@@ -224,6 +223,146 @@ describe('withList', () => {
                 children: [{ text: 'foo' }]
               }
             ]
+          }
+        ]);
+      });
+    });
+
+    describe('increaseItemDepth', () => {
+      test('should work for empty list item', () => {
+        inputText(editor, '- ');
+        editor.increaseItemDepth();
+
+        const nodes = [
+          {
+            type: F.BULLETED_LIST,
+            children: [
+              {
+                type: F.LIST_ITEM,
+                parent: F.BULLETED_LIST,
+                level: 1,
+                children: [{ text: '' }]
+              }
+            ]
+          }
+        ];
+
+        expect(editor.children).toStrictEqual(nodes);
+      });
+
+      test('should work for non-empty list item', () => {
+        inputText(editor, '- test');
+        editor.increaseItemDepth();
+
+        const nodes = [
+          {
+            type: F.BULLETED_LIST,
+            children: [
+              {
+                type: F.LIST_ITEM,
+                parent: F.BULLETED_LIST,
+                level: 1,
+                children: [{ text: 'test' }]
+              }
+            ]
+          }
+        ];
+
+        expect(editor.children).toStrictEqual(nodes);
+      });
+
+      test('should work with multiple items', () => {
+        inputText(editor, '- foo\nbar\nbaz');
+
+        Transforms.select(editor, { path: [0, 1, 0], offset: 0 });
+        editor.increaseItemDepth();
+
+        Transforms.select(editor, { path: [0, 2, 0], offset: 1 });
+        editor.increaseItemDepth();
+        editor.increaseItemDepth();
+
+        const nodes = [
+          {
+            type: F.BULLETED_LIST,
+            children: [
+              {
+                type: F.LIST_ITEM,
+                parent: F.BULLETED_LIST,
+                level: 0,
+                children: [{ text: 'foo' }]
+              },
+              {
+                type: F.LIST_ITEM,
+                parent: F.BULLETED_LIST,
+                level: 1,
+                children: [{ text: 'bar' }]
+              },
+              {
+                type: F.LIST_ITEM,
+                parent: F.BULLETED_LIST,
+                level: 2,
+                children: [{ text: 'baz' }]
+              }
+            ]
+          }
+        ];
+
+        expect(editor.children).toStrictEqual(nodes);
+      });
+    });
+
+    describe('decreaseItemDepth', () => {
+      test('should do nothing with level 0 item', () => {
+        inputText(editor, '- test');
+        editor.decreaseItemDepth();
+
+        const nodes = [
+          {
+            type: F.BULLETED_LIST,
+            children: [
+              {
+                type: F.LIST_ITEM,
+                parent: F.BULLETED_LIST,
+                level: 0,
+                children: [{ text: 'test' }]
+              }
+            ]
+          }
+        ];
+
+        expect(editor.children).toStrictEqual(nodes);
+      });
+
+      test('should correctly decrease depth', () => {
+        const listItem = {
+          type: F.LIST_ITEM,
+          parent: F.BULLETED_LIST,
+          level: 2,
+          children: [{ text: 'test' }]
+        };
+
+        editor.children = [
+          {
+            type: F.BULLETED_LIST,
+            children: [listItem]
+          }
+        ];
+
+        Transforms.select(editor, { path: [0, 0, 0], offset: 2 });
+
+        editor.decreaseItemDepth();
+        expect(editor.children).toStrictEqual([
+          {
+            type: F.BULLETED_LIST,
+            children: [{ ...listItem, level: 1 }]
+          }
+        ]);
+
+        editor.decreaseItemDepth();
+        expect(editor.children).toStrictEqual([
+          {
+            type: F.BULLETED_LIST,
+            children: [{ ...listItem, level: 0 }]
           }
         ]);
       });
